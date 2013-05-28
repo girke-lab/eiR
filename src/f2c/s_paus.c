714d1e7fd622434355f4117cd95ccdc1fbdd3f9d
k.horan <k.horan@bc3139a8-67e5-0310-9ffc-ced21a209358>
2013-05-28 16:25:56 +0000

    more fixes for windows
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/eiR@76932 bc3139a8-67e5-0310-9ffc-ced21a209358

--- src/f2c/s_paus.c
+++ src/f2c/s_paus.c
@@ -50,47 +50,47 @@ s_1paus(FILE *fin)
 		exit(0);
 		}
 	}
-
- int
-#ifdef KR_headers
-s_paus(s, n) char *s; ftnlen n;
-#else
-s_paus(char *s, ftnlen n)
-#endif
-{
-	fprintf(stderr, "PAUSE ");
-	if(n > 0)
-		fprintf(stderr, " %.*s", (int)n, s);
-	fprintf(stderr, " statement executed\n");
-	if( isatty(fileno(stdin)) )
-		s_1paus(stdin);
-	else {
-#ifdef MSDOS
-		FILE *fin;
-		fin = fopen("con", "r");
-		if (!fin) {
-			fprintf(stderr, "s_paus: can't open con!\n");
-			fflush(stderr);
-			exit(1);
-			}
-		s_1paus(fin);
-		fclose(fin);
-#else
-		fprintf(stderr,
-		"To resume execution, execute a   kill -%d %d   command\n",
-			PAUSESIG, getpid() );
-		signal1(PAUSESIG, waitpause);
-		fflush(stderr);
-		pause();
-#endif
-		}
-	fprintf(stderr, "Execution resumes after PAUSE.\n");
-	fflush(stderr);
-	return 0; /* NOT REACHED */
-#ifdef __cplusplus
-	}
-#endif
-}
-#ifdef __cplusplus
-}
-#endif
+//
+// int
+//#ifdef KR_headers
+//s_paus(s, n) char *s; ftnlen n;
+//#else
+//s_paus(char *s, ftnlen n)
+//#endif
+//{
+//	fprintf(stderr, "PAUSE ");
+//	if(n > 0)
+//		fprintf(stderr, " %.*s", (int)n, s);
+//	fprintf(stderr, " statement executed\n");
+//	if( isatty(fileno(stdin)) )
+//		s_1paus(stdin);
+//	else {
+//#ifdef MSDOS
+//		FILE *fin;
+//		fin = fopen("con", "r");
+//		if (!fin) {
+//			fprintf(stderr, "s_paus: can't open con!\n");
+//			fflush(stderr);
+//			exit(1);
+//			}
+//		s_1paus(fin);
+//		fclose(fin);
+//#else
+//		fprintf(stderr,
+//		"To resume execution, execute a   kill -%d %d   command\n",
+//			PAUSESIG, getpid() );
+//		signal1(PAUSESIG, waitpause);
+//		fflush(stderr);
+//		pause();
+//#endif
+//		}
+//	fprintf(stderr, "Execution resumes after PAUSE.\n");
+//	fflush(stderr);
+//	return 0; /* NOT REACHED */
+//#ifdef __cplusplus
+//	}
+//#endif
+//}
+//#ifdef __cplusplus
+//}
+//#endif
