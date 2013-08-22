c14adf29008481fa65166f5ec4dd16a82a59f16d
k.horan <k.horan@bc3139a8-67e5-0310-9ffc-ced21a209358>
2013-08-22 21:45:35 +0000

    fixed bug with default connections
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/eiR@79739 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/measures.R
+++ R/measures.R
@@ -25,7 +25,9 @@ defaultConn <- function(dir="."){
 	conn=eiOptions$defaultConn
 	if(is.null(conn) && file.exists(file.path(dir,ChemDb))){
 		conn = initDb(file.path(dir,ChemDb))
-		eiOptions$defaultConn=conn
+		# if we set this, the examples fail because the DBI packages are unloaded
+		# but not re-loaded because we won't re-call initDb if we already have a connection
+		#eiOptions$defaultConn=conn
 	}
 	conn
 }
@@ -39,8 +41,10 @@ addTransform <- function(descriptorType,compoundFormat=NULL,toString=NULL,toObje
 	name = buildType(descriptorType,compoundFormat)
 
 	if(!is.null(compoundFormat) && is.null(toString))
-		toString = function(input,dir=".") 
-			getTransform(descriptorType)$toString(getTransform(descriptorType,compoundFormat)$toObject(input)$descriptors)
+		toString = function(input,dir=".",conn=defaultConn()) 
+			getTransform(descriptorType)$toString(getTransform(
+																	descriptorType,compoundFormat)$toObject(input,conn,dir)$descriptors,
+															  dir=dir)
 	else if(is.null(toString))
 		stop("toString function must be specified if compoundFormat is NULL")		
 
@@ -51,28 +55,27 @@ addTransform <- function(descriptorType,compoundFormat=NULL,toString=NULL,toObje
 		# add extra handlers for compound_id and name types
 		addTransform(name,"compound_id",
 			# compound_id -> ap string
-			toString = function(ids,dir="."){
-				getDescriptors(initDb(file.path(dir,ChemDb)),name,ids)
+			toString = function(ids,conn,dir="."){
+				getDescriptors(conn,name,ids)
 			},
 			# compound_id -> AP list object
-			toObject = function(ids,dir="."){
-				descInfo = getTransform(name,"compound_id")$toString(ids,dir)
+			toObject = function(ids,conn,dir="."){
+				descInfo = getTransform(name,"compound_id")$toString(ids,conn,dir)
 				list(names=names(descInfo),
-					  descriptors=getTransform(name)$toObject(descInfo))
+					  descriptors=getTransform(name)$toObject(descInfo,conn,dir))
 			}
 		)
 
 		addTransform(name,"name",
 			# name -> ap string
-			toString = function(names,dir="."){
-				conn=initDb(file.path(dir,ChemDb))
+			toString = function(names,conn,dir="."){
 				getDescriptors(conn,name,findCompoundsByName(conn,names,keepOrder=TRUE))
 			},
 			# name -> AP list object
-			toObject = function(names,dir="."){
-				descInfo = getTransform(name,"name")$toString(names,dir)
+			toObject = function(names,conn,dir="."){
+				descInfo = getTransform(name,"name")$toString(names,conn,dir)
 				list(names=names,
-					  descriptors=getTransform(name)$toObject(descInfo))
+					  descriptors=getTransform(name)$toObject(descInfo,conn,dir))
 			}
 		)
 	}	
@@ -98,7 +101,7 @@ buildType <- function(descriptorType,compoundFormat) {
 
 addTransform("ap","sdf",
 	# Any sdf source -> APset
-	toObject = function(input,dir="."){
+	toObject = function(input,conn=NULL,dir="."){
 		sdfset=if(is.character(input) && file.exists(input)){
 			read.SDFset(input)
 		}else if(inherits(input,"SDFset")){
@@ -112,19 +115,19 @@ addTransform("ap","sdf",
 
 addTransform("fp","sdf",
 	# Any sdf source -> FPset
-	toObject = function(input,dir="."){
-		apList = getTransform("ap","sdf")$toObject(input,dir)
+	toObject = function(input,conn=NULL,dir="."){
+		apList = getTransform("ap","sdf")$toObject(input,dir=dir)
 		apList$descriptors = desc2fp(apList$descriptors)
 		apList
 	}
 )
 addTransform("ap",  
    # APset -> string,
-	toString = function(apset,dir="."){
+	toString = function(apset,conn=NULL,dir="."){
 		unlist(lapply(ap(apset), function(x) paste(x,collapse=", ")))
 	},
    # string or list -> AP set list
-	toObject= function(v,dir="."){ 
+	toObject= function(v,conn=NULL,dir="."){ 
 		if(inherits(v,"list") || length(v)==0)
 			return(v)
 
@@ -137,11 +140,11 @@ addTransform("ap",
 )
 addTransform("fp",  
    # FPset -> string,
-	toString = function(fpset,dir="."){
+	toString = function(fpset,conn=NULL,dir="."){
 		sapply(1:length(fpset), function(i) as(fpset[i],"character") )
 	},
    # string or list -> FP set list
-	toObject= function(v,dir="."){ 
+	toObject= function(v,conn=NULL,dir="."){ 
 		if(inherits(v,"list") || length(v)==0)
 			return(v)
 
